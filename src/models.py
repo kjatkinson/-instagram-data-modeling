d30ce35d63b460865b6ef972d2339616d4672300
kjatkinson <149319433+kjatkinson@users.noreply.github.com>
2024-04-11 00:46:02 +0000

    data

--- src/models.py
+++ src/models.py
@@ -1,30 +1,70 @@
 import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, declarative_base
-from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Follower(Base):
+    __tablename__ = 'follower'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    comments = relationship("Comment")
+    posts = relationship("Post")
+    media = relationship("Media")
+    def to_dict(self):
+        return {}
+
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     street_name = Column(String(250))
     street_number = Column(String(250))
     post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
 
+    comments = relationship("Comment")
+    posts = relationship("Post")
+    media = relationship("Media")
+    def to_dict(self):
+        return {}
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    text = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+    comments = relationship("Comment,followers")
+    posts = relationship("Post, followers")
+    media = relationship("Media, followers")
+    def to_dict(self):
+        return {}
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+    comments = relationship("Comment")
+    posts = relationship("Post")
+    media = relationship("Media")
+    def to_dict(self):
+        return {}
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(250))
+    url = Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+    comments = relationship("Comment")
+    posts = relationship("Post")
+    media = relationship("Media")
     def to_dict(self):
         return {}
 
@@ -33,5 +73,5 @@ try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
-    print("There was a problem genering the diagram")
+    print("There was a problem generating the diagram")
     raise e
